174bdeb9256295c163eb5ce21730bba4ed778fef
sheng-23 <1207942059@qq.com>
2020-09-04 15:36:38 +0800

    git操作步骤

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-1. git init
+/*
+git config --global user.name "github的用户名"
+git config --global user.email "github的邮箱"
+
+
+1. git init 
+		// 把文件托管给 git
+2. git add . 
+		// 保存代码
+3. git commit -m 'master' 
+		// 提交代码
+4. git remote add origin https://github.com/sheng-23/root.git 
+		// 拷贝自己的这行代码 创建仓库
+5. git push -u origin master 
+		// 提交代码到 github 
+
+// -------------------------------
+
+// 正常进公司以后步骤
+1. 创建公钥 私钥
+ssh-keygen -t rsa -C "你的github邮箱"
+2. 打开公钥
+cat ~/.ssh/id_rsa.pub\
+
+git status
+	// 查看哪些文件修改了
+
+
+
+*/
