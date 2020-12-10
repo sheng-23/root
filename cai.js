7ae94aef278374d112e93e53d11ffc593e8fc0e1
shengsheng.feng <shengsheng.feng@daoyitong.com>
2020-12-10 17:33:30 +0800

    小 逻辑题

--- cai.js
+++ cai.js
@@ -1,26 +1,32 @@
-let ar = {
-  title: '1234',
-  title: '5678',
-  title: [
-    {
-      title2: 'zxc',
-    },
-    {
-      title2: 'asd',
-    },
-    {
-      title2: 'qwe',
-    },
-  ]
-}
-console.log(ar[0].title)
-// function fun(ar) {
-//   return ar.map((item, index) => {
-//     if(item.title){
-//       return 
-//     }
-//     return {
+let ar = [
+  { title: '1234', },
+  { title: '5678', },
+  {
+    title: '9012',
+    child: [
+      {
+        title2: 'zxc',
+      },
+      {
+        title2: 'asd',
+      },
+      {
+        title2: 'qwe',
+      },
+    ]
+  }
+]
 
-//     }
-//   })
-// }
+function fun(ar) {
+  let str = ''
+  ar.forEach((item, index) => {
+    if(Object.prototype.toString.call(item.child).includes('Array')){
+      fun(item.child)
+    }else{
+      str += (item.title || item.title2)
+    }
+  })
+  return str
+}
+let cr = fun(ar)
+console.log(cr)
